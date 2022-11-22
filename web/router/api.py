e369dcb3bd7ad8a36c0432b1d8db8d0ddfacac35
thao thao <thaovuthikimthao@gmail.com>
2022-11-22 23:19:24 +0700

    Fix server error

--- web/router/api.py
+++ web/router/api.py
@@ -1,4 +1,3 @@
-from sys import prefix
 from fastapi import APIRouter
 from router import thegioididong
 from router import shoppe
@@ -9,12 +8,12 @@ from router import api_app
 from router import label
 
 api = APIRouter()
-api.include_router(lazada.app , tags=["lazada"] , prefix = "/api")
-api.include_router(thegioididong.app , tags=["thegioididong"] , prefix = "/api")
-api.include_router(shoppe.app , tags=["shoppe"] , prefix = "/api")
-api.include_router(lazada.app , tags=["lazada"] , prefix = "/api")
-api.include_router(showproduct.app , tags=["goc"] , prefix = "/api")
-api.include_router(fptshop.app , tags=["fpt"] , prefix = "/api")
-api.include_router(api_app.app , tags = ["api_web"] , prefix = "/api")
-api.include_router(label.app , tags=["label"] , prefix = "/api")
+api.include_router(lazada.app ,         prefix = "/api",    tags=["lazada"])
+api.include_router(thegioididong.app ,  prefix = "/api",    tags=["thegioididong"])
+api.include_router(shoppe.app ,         prefix = "/api",    tags=["shoppe"])
+api.include_router(lazada.app ,         prefix = "/api",    tags=["lazada"])
+api.include_router(showproduct.app ,    prefix = "/api",    tags=["goc"])
+api.include_router(fptshop.app ,        prefix = "/api",    tags=["fpt"])
+api.include_router(api_app.app ,        prefix = "/api",    tags=["api_web"])
+api.include_router(label.app ,          prefix = "/api",    tags=["label"])
 
