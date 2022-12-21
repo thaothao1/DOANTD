451b211f205e9779c69353ba6636d02b1a52634b
ThuDieu2708 <thudieu27082000@gmail.com>
2022-12-22 02:31:50 +0700

    update data(2  -> 1)

--- web/router/showproduct.py
+++ web/router/showproduct.py
@@ -77,7 +77,7 @@ def getName(nameOne , nameTwo):
     for i in range(len(shopTwo)):
         if shopTwo[i] in shopOne:
             count+=1
-    if (count >= len(shopOne)-2):
+    if (count >= len(shopOne)-1):
         return True
     else:
         return False
